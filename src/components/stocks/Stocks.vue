d9afaa3939896ca86c8e3e6dcf80cf787f1f0d64
Milan <mistikone1@gmail.com>
2020-07-03 14:28:32 +0200

    stock/stocks components

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,13 +1,34 @@
 <template>
-  <h1>Stocks</h1>
+  <div>
+    <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
+  </div>
 </template>
 
 <script>
-export default {
+import Stock from "./Stock";
 
-}
+export default {
+  components: {
+    appStock: Stock
+  },
+  computed: {
+    stocks() {
+      return this.$store.getters.stocks;
+    }
+  }
+
+  // data() { //MOVED TO stock.js in data folder
+  //   return {
+  //     stocks: [
+  //       { id: 1, name: "BMW", price: 110 },
+  //       { id: 2, name: "Google", price: 200 },
+  //       { id: 3, name: "Apple", price: 250 },
+  //       { id: 4, name: "Twitter", price: 8 }
+  //     ]
+  //   };
+  // }
+};
 </script>
 
 <style>
-
 </style>
