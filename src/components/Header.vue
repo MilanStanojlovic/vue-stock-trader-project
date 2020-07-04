f4c12eaaf4b6be0f202ffc12963377975897c104
Milan <mistikone1@gmail.com>
2020-07-04 12:38:13 +0200

    randomize stock price and animating page transition

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,11 +13,16 @@
             <a>Stoks</a>
           </router-link>
         </ul>
+        <strong class="navbar-text navbar-right">Funds: {{ funds | currency}}</strong>
         <ul class="nav navbar-nav navbar-right">
           <li>
-            <a href="#">End Day</a>
+            <a href="#" @click="endDay">End Day</a>
           </li>
-          <li class="dropdown">
+          <li
+            class="dropdown"
+            :class="{open: isDropdownOpen}"
+            @click="isDropdownOpen = !isDropdownOpen"
+          >
             <a
               href="#"
               class="dropdown-toggle"
@@ -47,7 +52,28 @@
 </template>
 
 <script>
-export default {};
+import { mapActions } from "vuex";
+
+export default {
+  data() {
+    return {
+      isDropdownOpen: false
+    };
+  },
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    }
+  },
+
+  methods: {
+    ...mapActions(["randomizeStocks"]),
+
+    endDay() {
+      this.randomizeStocks();
+    }
+  }
+};
 </script>
 
 <style>
