7278bfe55b104974e638dcc811042b1a209ffe50
Milan <mistikone1@gmail.com>
2020-07-05 11:50:03 +0200

    put stock data vuex

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -36,10 +36,10 @@
             </a>
             <ul class="dropdown-menu">
               <li>
-                <a href="#">Save Data</a>
+                <a href="#" @click="saveData">Save Data</a>
               </li>
               <li>
-                <a href="#">Load Data</a>
+                <a href="#" @click="loadData">Load Data</a>
               </li>
             </ul>
           </li>
@@ -67,10 +67,26 @@ export default {
   },
 
   methods: {
-    ...mapActions(["randomizeStocks"]),
+    ...mapActions({
+      randomizeStocks: "randomizeStocks",
+      fetchData: "loadData"
+    }),
 
     endDay() {
       this.randomizeStocks();
+    },
+
+    saveData() {
+      const data = {
+        funds: this.$store.getters.funds,
+        stockPortfolio: this.$store.getters.stockPortfolio,
+        stocks: this.$store.getters.stocks
+      };
+      this.$http.put("data.json", data);
+    },
+
+    loadData() {
+      this.fetchData();
     }
   }
 };
